49a23fd22990f189244183d1062a1eb49daaf4f9
Lisa <elisaveta.suchkova@gmail.com>
2022-11-15 11:27:47 +0200

    last commit

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-url = "postgresql+psycopg2://liza:liza_kisya@barbak.cyou:5432/liza"
 
 engine = create_engine(url)
 Base = declarative_base()
